9d6eb3cde9f56c93cd961543c68125a788e14b32
Dementor <gk1993@rambler.ru>
2018-07-17 15:14:53 +0100

    Моя первая программа

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 var profit = +prompt("Ваш бюджет?");
-		shopName = prompt("Название Вашего магазина?");
+var	shopName = prompt("Название Вашего магазина?");
 
 var quest1 = prompt("Какой тип товаров будем продавать?");
 var quest2 = prompt("Какой тип товаров будем продавать?");
 var quest3 = prompt("Какой тип товаров будем продавать?");
 
-employersObj = {
-		name: "Sari",
-		age: "42",
-		work: "manager"
-	}
+var employersOp = {
+	name: "Sari",
+	age: "42",
+	work: "manager"
+};
 
-employers = 
-
-mainList = {
+var mainList = {
 	summa: profit,
 	nameShop: shopName,
 	shopGoods: [quest1, quest2, quest3],
-	employers: employersObj
-}
+	employers: employersOp
+};
+console.log(mainList.employers);
 
-alert("Ваш бюджет на 1 день: " + mainList.summa/30);
+alert("Ваш бюджет на 1 день: " + mainList.summa/30 + " рублей.");
