4b0e1690111836445d508538be52bab209899a87
Dementor <gk1993@rambler.ru>
2018-07-17 14:29:40 +0100

    Первый урок

---
+++ script.js
@@ -0,0 +1,23 @@
+var profit = +prompt("Ваш бюджет?");
+		shopName = prompt("Название Вашего магазина?");
+
+var quest1 = prompt("Какой тип товаров будем продавать?");
+var quest2 = prompt("Какой тип товаров будем продавать?");
+var quest3 = prompt("Какой тип товаров будем продавать?");
+
+employersObj = {
+		name: "Sari",
+		age: "42",
+		work: "manager"
+	}
+
+employers = 
+
+mainList = {
+	summa: profit,
+	nameShop: shopName,
+	shopGoods: [quest1, quest2, quest3],
+	employers: employersObj
+}
+
+alert("Ваш бюджет на 1 день: " + mainList.summa/30);
